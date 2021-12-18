7e276848f8eefc9ae75d53985034f6f760a24c99
Mecid(Majd ) elhasan <96142959+majd-elhasan@users.noreply.github.com>
2021-12-18 18:43:31 +0300

    If-ElseIf-Ternary-If

---
+++ Program6.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace If_ElseIf_Ternary_If
+{
+    class Program6
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+            if (time > 5 && time<10){Console.WriteLine("Günaydın !");}
+            else if(time >= 10 && time <18 ){Console.WriteLine("iyi günler !");}
+            else Console.WriteLine("iyi geceler !");
+
+            /// yada
+            string s_time = time >5 && time <10 ? "Günaydın !":time >= 10 && time <18?"iyi günler !": "iyi geceler !";
+            Console.WriteLine(s_time);
+        }
+    }
+}
