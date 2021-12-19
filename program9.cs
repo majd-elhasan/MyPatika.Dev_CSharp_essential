94342566b54ef902b4582570d50c09fed87b2f84
Mecid(Majd ) elhasan <96142959+majd-elhasan@users.noreply.github.com>
2021-12-19 14:52:55 +0300

    while_foeeach_program

---
+++ program9.cs
@@ -0,0 +1,56 @@
+﻿using System;
+ namespace WhileForeach
+ {
+     class while_foeeach_program
+     {
+        static void Main(string[] args)
+        {
+            //while 
+            // 1 den başlayıp consol dan girilen sayıya kadar ortalama hesaplayıp console a yazdıran program
+            Console.WriteLine("birden kendisne kadar ortalama hesaplamak istediğiniz bir sayı giriniz !");
+            Console.WriteLine("");
+            int sayac = 1;
+            int Toplam = 0;
+            int sayi =0;
+            string girdi = null;
+            try
+            {
+            girdi = Console.ReadLine();
+            sayi = int.Parse(girdi);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Lütfen bir sayı giriniz !");
+            }
+            if (sayi !=0)
+            {
+            while(sayac<=sayi)
+            {
+                Toplam +=sayac;
+                sayac ++;
+                
+                
+            } 
+            float Toplamf = Toplam;
+            float average = Toplamf/sayi;
+            //Console.WriteLine(3/2);
+            Console.WriteLine("1 den "+ sayi +" a kadar ortalama : " + average);
+            }
+            else if(girdi =="0") Console.WriteLine("Lütfen 0 dan büyük bir sayı giriniz !");
+            Console.WriteLine("**************CharacterLoop****************");
+            char Character = 'a';
+            while (Character <= 'z')
+            {
+                Console.Write(Character + " ");
+                Character ++ ;
+            }
+            Console.WriteLine("");
+            Console.WriteLine("************ForEachLoop*************");
+            string[] arabalar = {"Hundai","Nissan","voxwagen","Tofaş"};
+            foreach (var item in arabalar)
+            {
+                Console.WriteLine(item);
+            }
+        }
+     }
+ }
