3acfe5ac87cd0bf95a4e3eb18823f8fb43b9fb81
Mecid(Majd ) elhasan <96142959+majd-elhasan@users.noreply.github.com>
2021-12-21 21:51:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+ namespace Metot_tanimlama
+ {
+     class Metot_tanimlama_Program
+     {
+        static void Main(string[] args)
+        {
+            //erişim belirtici geridönüş_tipi  metodun_adı(paramitre_listesi/arguman)
+            //{
+                //return
+            //}
+            int a = 2;
+            int b = 3;
+            
+            
+            Metotlar metotlar = new Metotlar();
+            metotlar.ArttırVeTopla_yerineYaz_(ref a,ref b);
+            int sonuc = Topla(a,b);
+            metotlar.EkranaYazdır(Convert.ToString(sonuc));
+            
+        }
+        static int Topla(int deger1 , int deger2)
+        {
+            return deger1+deger2;
+        }
+     }
+     class Metotlar
+     {
+         public void EkranaYazdır(string veri)
+         {
+             Console.WriteLine(veri);
+         }
+         public int ArttırVeTopla(int deger1, int deger2)
+         {
+             deger1 ++; deger2++;
+             return deger1 + deger2;
+         }
+         //reference ↓
+          public int ArttırVeTopla_yerineYaz_(ref int deger1,ref int deger2)
+         {
+             deger1 ++; deger2++;
+             return deger1 + deger2;
+         }
+     }
+ }
