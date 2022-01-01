27ee70b06d39cddb865bef0698e1593f52a3fe84
Mecid(Majd ) elhasan <96142959+majd-elhasan@users.noreply.github.com>
2022-01-01 16:27:05 +0300

    Add files via upload

---
+++ Static_sinif_ve_uyeler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+ namespace Static_sinif_ve_uyeler
+ {
+     class MainClass
+     {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*******************************************");
+            Console.WriteLine("Çalışan Sayısı : {0}" ,calisan.CalisanSayisi);
+            calisan calisan_1 = new calisan("Ayşe","yılmaz","IT");
+            Console.WriteLine("Çalışan Sayısı : {0}" ,calisan.CalisanSayisi);
+            calisan calisan_2 = new calisan("Mecid","Elhasan","IT");
+            Console.WriteLine("Çalışan Sayısı : {0}" ,calisan.CalisanSayisi);
+            Console.WriteLine(islemler.Topla(12,9));
+            Console.WriteLine(islemler.cikar(12,9));
+        }
+     }
+     class calisan
+     {
+        private static int calisanSayisi = 0;
+
+        public static int CalisanSayisi { get => calisanSayisi;}
+        private string isim;
+        private string soyisim;
+        private string depatman;
+        //static calisan() => calisanSayisi = 0;
+        public calisan(string Isim, string Soyisim,string Departman)
+        {
+            this.isim = Isim;
+            this.soyisim = Soyisim;
+            this.depatman = Departman;
+            calisanSayisi ++;
+        }
+
+    }
+    static class islemler
+    {
+        public static long Topla(int value1 , int value2)
+        {
+            return value1 + value2;
+        }
+         public static long cikar(int value1 , int value2)
+        {
+            return value1 - value2;
+        }
+    }
+
+ }
